636e17627d16d788a2dff6e6ffcbbbb3f0e1e180
sirAges <ekele19stephen96u@gmail.com>
2025-05-05 11:40:22 +0100

    first commit

--- prisma/migrations/20250503141726_init/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "Business" ALTER COLUMN "verificationId" DROP NOT NULL,
-ALTER COLUMN "certificateUrl" DROP NOT NULL;
--- prisma/migrations/20250504012726_init/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `certificateUrl` on the `Business` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Business" DROP COLUMN "certificateUrl",
-ADD COLUMN     "certificateFile" JSONB;
